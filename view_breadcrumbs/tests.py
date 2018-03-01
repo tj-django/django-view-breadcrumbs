b1836f566b17807a55df4afc633650bd6ba035f9
Tonye Jack <tonye@coherentpath.com>
2018-02-28 19:33:22 -0500

    Added test config for tox, pytest, travis CI

--- view_breadcrumbs/tests.py
+++ view_breadcrumbs/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+class BaseBreadcrumbTestCase(TestCase):
+
+    def setUp(self):
+        self.crumbs = []
+
+    def test_empty_crumbs(self):
+        self.assertEqual(0, len(self.crumbs))
