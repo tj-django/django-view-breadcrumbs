77cc043932351f90a768231e6f02aa7b6635d77b
Tonye Jack <tonye@coherentpath.com>
2018-08-19 16:57:49 -0400

    Updated setup

--- conftest.py
+++ conftest.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from django import setup
 from django.conf import settings
@@ -28,3 +29,21 @@ def pytest_configure():
         ]
     )
     setup()
+    create_db()
+
+
+def create_db():
+    if (sys.version_info < (3, 6)):
+        from django.db import connection
+
+        with connection.cursor() as c:
+            c.executescript(
+            """
+            BEGIN;
+            --
+            -- Create model TestModel
+            --
+            DROP TABLE  IF EXISTS "demo_testmodel";
+            CREATE TABLE "demo_testmodel" ("created_at" datetime NOT NULL, "id" integer NOT NULL PRIMARY KEY AUTOINCREMENT, "update_at" datetime NOT NULL, "name" varchar(50) NOT NULL);
+            COMMIT;
+            """)
