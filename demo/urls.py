37965ef9814a822cc4349a7c7ad99d77b118c806
Tonye Jack <tonye@coherentpath.com>
2019-01-13 08:25:58 -0500

    Updated the test class.

--- demo/urls.py
+++ demo/urls.py
@@ -2,7 +2,20 @@ from django.urls import path
 
 from . import views
 
+"""
+Examples:
+    For the events LIST and CRUD views.
+    [
+        path('events/', views.EventLists.as_view(), name='event_list'),
+        path('event/add/', views.EventCreate.as_view(), name='event_add'),
+        path('event/<int:pk>/change', views.EventUpdate.as_view(), name='event_change'),
+        path('event/<int:pk>/delete', views.EventDelete.as_view(), name='event_delete'),
+        path('event/<int:pk>/delete_error', views.EventDeleteError.as_view(), name='event_delete_error'),
+    ]
+"""
+
+app_name = 'demo'
 
 urlpatterns = [
-   path('list/', views.TestView.as_view(), name='test_view'),
+   path('test-view/', views.TestView.as_view(), name='test_view'),
 ]
