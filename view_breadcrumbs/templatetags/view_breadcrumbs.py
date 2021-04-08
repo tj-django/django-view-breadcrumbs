6826e930b5765e4b7cc7f3d3f501249b9f21287f
Tonye Jack <jtonye@ymail.com>
2021-04-08 19:01:52 -0400

    Fixed errors.

--- view_breadcrumbs/templatetags/view_breadcrumbs.py
+++ view_breadcrumbs/templatetags/view_breadcrumbs.py
@@ -150,13 +150,17 @@ def _view_url(model, suffix):
 
 @register.simple_tag()
 def list_view_url(model, suffix=LIST_VIEW_SUFFIX):
-    assert model is not None, "Invalid model"
+    if model is None:
+        raise ValueError("Invalid model")
+
     return _view_url(model=model, suffix=suffix)
 
 
 @register.simple_tag()
 def create_view_url(model, suffix=CREATE_VIEW_SUFFIX):
-    assert model is not None, "Invalid model"
+    if model is None:
+        raise ValueError("Invalid model")
+
     return _view_url(model=model, suffix=suffix)
 
 
