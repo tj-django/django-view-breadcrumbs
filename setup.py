ed0383164e21839d54d5fa179b2cc552c8cb208c
Tonye Jack <jtonye@ymail.com>
2021-11-07 06:57:25 -0500

    Bump version: 2.2.3 → 2.2.4

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ else:
 setup(
     name="django-view-breadcrumbs",
     python_requires=">=3.6",
-    version="2.2.3",
+    version="2.2.4",
     author="Tonye Jack",
     author_email="jtonye@ymail.com",
     long_description=LONG_DESCRIPTION,
