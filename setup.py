8c447a935698b82d302b85643b5dbdd51cc18f6f
Tonye Jack <jtonye@ymail.com>
2021-11-07 06:54:42 -0500

    Revert version change.

--- setup.py
+++ setup.py
@@ -69,7 +69,7 @@ else:
 setup(
     name="django-view-breadcrumbs",
     python_requires=">=3.6",
-    version="2.2.3",
+    version="2.2.2",
     author="Tonye Jack",
     author_email="jtonye@ymail.com",
     long_description=LONG_DESCRIPTION,
